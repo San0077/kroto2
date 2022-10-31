7278aac515e4b0371247c1d97d57b3edc7074f7c
san0077 <santhoshbalaji304@gmail.com>
2022-10-31 10:25:52 +0530

    customer laps

--- src/App.js
+++ src/App.js
@@ -1,86 +1,157 @@
 import './App.css';
-import React,{useState} from 'react';
+import React,{useEffect, useRef, useState} from 'react';
 import {Routes, Route,useNavigate ,Navigate} from 'react-router-dom';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { useEffect } from 'react';
-import { useCallback } from 'react';
+import { ChakraProvider, Drawer,Input , DrawerCloseButton, DrawerContent, DrawerOverlay, useDisclosure, Select, DrawerFooter } from '@chakra-ui/react';
+import { SizeExample } from './drawer';
+import { Button } from 'react-bootstrap';
 
+var valueName=[]
  function App() {
-  const obj=[
-    "electronics",
-    "jewelery",
-    "men's clothing",
-    "women's clothing"
-  ];
+  const [size, setSize] = useState('')
+  const [dropdown, setdropdown] = useState([]);
+  const [segmentName, setsegmentName] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const previousValue = useRef("")
+  const [inputValue, setInputValue] = useState("");
 
-  const card =[]
-  const [details, setdetails] = useState();
-  const [open, setopen] = useState(false);
-
-    useEffect(()=>{
-      console.log("rendering")
-      for(let i=0;i<obj.length;i++){
-        fetch(`https://fakestoreapi.com/products/category/${obj[i]}`).then(data=>data.json())
-        .then( data=>{
-             card.push(data[0])
-            if(i=="3"){
-              setopen(true)
-              setdetails(card)
-            }
-            
-        })
-       }
-    },[])
-    
-      const add=useCallback((d)=>{
-        fetch(`https://fakestoreapi.com/products/category/${d.category}`).then(data=>data.json())
-        .then(data=>{
-              const result= data.find(data=>data.id==d.id)
-              var index=data.indexOf(result)
-              index = index+1;
-              if(index >= data.length){
-                alert("no more data of this category")
-              }else{  setdetails([...details,data[index]])}
-            
-        })
-      },[details])
+  const handleClick = (newSize) => {
+    setSize(newSize)
+    onOpen()
+  }
+ 
+  function textValue(e){
+          let obj ={
+          [e.target.value]:e.target.value
+          }
+          valueName.push(obj)
+  }
   
-  return (
-  <div className ="app">
-    { open?details.map(d=>
-    <div>
-      
-  <Card sx={{ maxWidth: 300,}}>
-  <Typography gutterBottom variant="h5" component="div">
-          {d.category}
-        </Typography>
-      <CardMedia
-        component="img"
-        height="140"
-        image= {d.image}
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-        {d.title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" onClick={()=>add(d)}>ADD</Button>
-      </CardActions>
-    </Card>
+  
+  function edit(e){
+     let fil = dropdown.filter(element=>element!=e)
+     valueName.forEach(object => {
+      delete object[e];
+    });
+     setdropdown(fil)
+     const select =document.getElementById("trends")
+     select.innerHTML += `<option>${e}</option>`;
+  }
+   let box = dropdown.map(e=>{
+    
+    return(
+      <div>
+     <Select placeholder={e} id={e}    size='md' >
+                    <option value='firstName'>FirstName</option>
+                    <option value='lastName'>LastName</option>
+                    <option value='age'>Age</option>
+                    <option value='account Name'>Account Name</option>
+                    <option value='gender'>Gender</option>
+                    <option value='city'>City</option>
+                    <option value='state'>State</option>
+    </Select>
+    <button className="button2" onClick={() =>edit(e)}><i class="fa fa-minus" aria-hidden="true"></i></button>
     </div>
-    ):"loading"}
- </div>
+    )
+   })
+   const savetheSegment=()=>{
+    if(segmentName!=""){
+        let obj={
+          "segment_name":segmentName,
+          "schema":valueName
+        }
+        
+        fetch("https://webhook.site/6397e935-26e7-4911-8a41-990eb0ad52fd",{
+        method:"POST",
+        body: JSON.stringify(obj),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }).then(data=>data.json()).then(data=>console.log(data))
+    }
+        
+   }
+  const save =()=>{
+           const select =document.getElementById("trends")
+           for(let i=0 ; i<select.length;i++){
+                let trend = select[i]
+                if(trend.selected){
+                    setdropdown([...dropdown,trend.value])
+                    select.remove(i)
+                }
+           }
+  }
+  const sizes = 'md'
+  return (
+    <div>
+  <div className ="app">
+    <div>
+    <i class="fa fa-angle-left fa-2x" aria-hidden="true"></i>
+    </div>
+       <div className="view">
+           View Audience
+       </div>
+      
+   </div>
    
+   <button className="button" onClick={() => handleClick(size)}
+        key={size}> save segment</button>
+   <div className="slide">
+   <ChakraProvider>
+        <Drawer onClose={onClose} isOpen={isOpen} size={sizes}>
+        <DrawerOverlay />
+        <DrawerContent>
+       
+      <div>
+          <div className='app'>
+        <div>
+           <i class="fa fa-angle-left " aria-hidden="true"></i>
+        </div>
+        <div className="view">
+           saving segment
+        </div>
+          </div>
+             <div className="details">
+                  <label>Enter the Name of the Segment</label>
+                  <Input placeholder='Name of the segment' onChange={(e)=>setsegmentName(e.target.value)} size='md' />
+                  <p>
+                    To save the Segment you need to add the schemas to bulid the query
+                  </p>
+                  <div>
+                       <section><span className="circle"><i class="fa fa-circle" style={{color:"red",paddingRight:"10px"}} aria-hidden="true"></i></span>user traits</section>
+                       <section><span><i class="fa fa-circle" style={{color:"green",paddingRight:"10px"}} aria-hidden="true"></i></span>Group traits</section>
+                  </div>
+                   <div className="seletion">
+                        {box?box:""}
+                  
+                  <Select placeholder='Add schema to segment' onChange={(e)=>textValue(e)} id ="trends" size='md' >
+                    <option value='firstName'>FirstName</option>
+                    <option value='lastName'>LastName</option>
+                    <option value='age'>Age</option>
+                    <option value='account Name'>Account Name</option>
+                    <option value='gender'>Gender</option>
+                    <option value='city'>City</option>
+                    <option value='state'>State</option>
+                  </Select>
+                  <p onClick={() =>save()}>+ Add new Schema</p>
+                  </div>
+                  <section className="final">
+
+                  <button  onClick={() =>savetheSegment()}>save the segment</button>
+                  <button className="button2" onClick={onClose}>cancel</button>
+
+                  </section>
+
+              </div> 
+             
+          
+      
+        </div>
+     
+        </DrawerContent>
+      </Drawer>
+        </ChakraProvider>
+    </div>
+   </div>
   )
 }
 
